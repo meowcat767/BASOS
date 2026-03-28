b6821a6cdf2a0c9db1073f3783296b4cdb083d06
Ben <meowcat@meowcat.site>
2026-03-28 23:24:00 +0000

    idfk anymore

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,19 +1,22 @@
-// multiboot 2 header
-__attribute__((section(".multiboot")))
+// multiboot header
+__attribute__((section(".multiboot"), used))
 const unsigned int multiboot_header[] = {
     0x1BADB002,
     0x0,
     -(0x1BADB002)
 };
 
-// kernel load here
-void kernel_main()
-{
-    volatile char* vga = (char*)0xb8000;
+
+void kernel_main() {
+    volatile char* vga = (volatile char*)0xB8000;
     const char* msg = "Hello, World!";
-    for (int i = 0; msg[1] != 0; i++) {
-        vga[1 * 2] = msg[i];
-        vga[1 * 2 + 1] = 0x07;
+
+    for (int i = 0; msg[i] != 0; i++) {
+        vga[i * 2] = msg[i];
+        vga[i * 2 + 1] = 0x07;
     }
+
     while (1) {}
 }
+
+
