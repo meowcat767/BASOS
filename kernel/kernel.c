6e52a9eae728a59b180770f9c58b41806e9b5890
Ben <meowcat@meowcat.site>
2026-03-28 23:31:21 +0000

    make it safe

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 // multiboot header
 __attribute__((section(".multiboot"), used))
 const unsigned int multiboot_header[] = {
@@ -17,6 +19,53 @@ void kernel_main() {
     }
 
     while (1) {}
+}
+
+struct GDTEntry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t  base_middle;
+    uint8_t  access;
+    uint8_t  granularity;
+    uint8_t  base_high;
+} __attribute__((packed));
+
+struct GDTPtr {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+struct GDTEntry gdt[3];
+struct GDTPtr gdt_ptr;
+
+void gdt_install() {
+    gdt_ptr.limit = sizeof(gdt) - 1;
+    gdt_ptr.base = (uint32_t)&gdt;
+
+    // Null segment
+    gdt[0] = (struct GDTEntry){0,0,0,0,0,0};
+
+    // Code segment
+    gdt[1] = (struct GDTEntry){
+        .limit_low = 0xFFFF,
+        .base_low = 0,
+        .base_middle = 0,
+        .access = 0x9A,      // executable, readable, ring 0
+        .granularity = 0xCF,
+        .base_high = 0
+    };
+
+    // Data segment
+    gdt[2] = (struct GDTEntry){
+        .limit_low = 0xFFFF,
+        .base_low = 0,
+        .base_middle = 0,
+        .access = 0x92,      // writable, ring 0
+        .granularity = 0xCF,
+        .base_high = 0
+    };
+
+    asm volatile("lgdt %0" : : "m"(gdt_ptr));
 }
 
 
